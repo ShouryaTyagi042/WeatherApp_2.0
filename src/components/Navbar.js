589ad4aec02823487d677ffe285af3c7a40a3439
Shourya Tyagi <shouryatyagi042@gmail.com>
2023-04-04 11:30:45 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,10 +4,12 @@ import "../css/Navbar.css";
 
 function Navbar() {
   return (
-    <nav class="flex items-center justify-between flex-wrap bg-teal-500 p-6">
-      <div class="flex items-center flex-shrink-0 text-white mr-6">
-        <img src={Icon}></img>
-        <span class="font-semibold text-xl tracking-tight">Weather App</span>
+    <nav className="flex items-center justify-between flex-wrap bg-teal-500 p-6">
+      <div className="flex items-center flex-shrink-0 text-white mr-6">
+        <img src={Icon} alt="WeatherIcon"></img>
+        <span className="font-semibold text-xl tracking-tight">
+          Weather App
+        </span>
       </div>
     </nav>
   );
