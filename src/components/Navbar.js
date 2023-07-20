98d45e802d2e4b9c03bd25adf336885ddde31ba0
Shourya Tyagi <shouryatyagi042@gmail.com>
2023-07-20 15:21:49 +0530

    Changed navbar commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,7 @@ import "../css/Navbar.css";
 
 function Navbar() {
   return (
-    <nav className="flex items-center justify-between flex-wrap bg-teal-500 p-6">
+    <nav className="flex items-center justify-between flex-wrap bg-red-400 p-6">
       <div className="flex items-center flex-shrink-0 text-white mr-6">
         <img src={Icon} alt="WeatherIcon"></img>
         <span className="font-semibold text-xl tracking-tight">
